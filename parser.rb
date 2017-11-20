3c36eeed298c7a6440642123ab020a6ff5527b78
judeostorn <judeostorn66@gmail.com>
2017-11-20 13:09:57 +0500

    mech test

--- parser.rb
+++ parser.rb
@@ -1,8 +1,8 @@
-require 'open-uri'
-require 'nokogiri'
+require 'mechanize'
 
-page = Nokogiri::HTML(open('http://www.a-yabloko.ru/catalog'), encoding = 'cp1251')
-dock = File.open('test2.txt', 'w') { |f| f << page }
-#page.parse_file('test.txt', encoding = 'UTF-8') {|f| f << page.text }
-#если записывать текст то он автоматом меняет кодировку. page.text.to_s.encode("UTF-8")
-#если записывать весь html то такого результат не выходит. и русские символы слетают в "������"
+mechanize = Mechanize.new
+
+page = mechanize.get('http://www.a-yabloko.ru/catalog')
+
+#p page.class
+puts page.at('.goods').text# { |it| p it }
