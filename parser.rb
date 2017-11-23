41e5dffd6bbe9babb67255989180a75932769dde
judeostorn <judeostorn66@gmail.com>
2017-11-23 17:39:46 +0500

    upload items photo

--- parser.rb
+++ parser.rb
@@ -1,40 +1,48 @@
 require 'mechanize'
 
-#1) Тип сущности (группа, подгруппа, товар)
-#2) Наименование (группы, подгруппы, товара)
-#3) Группу товара (если данная строка предствляет товар)
-#4) Имя файла с изображением данного товара (при парсинге каталога, необходимо так же загружать изображения товара, давать файлам уникальные имена, и записывать имя файла в соответсвуюущую строчку в таблице-файле)
-#5) Условный идетификатор группы или товара (по которому вы сможете определить, были ли этот товар/группа загружены вашей программой)
-#После того, как первую 1000 товаров ваша программа загрузила, необходимо рассчитать статистику и вывести ее в консоль:
+# 1) Тип сущности (группа, подгруппа, товар)
+# 2) Наименование (группы, подгруппы, товара)
+# 3) Группу товара (если данная строка предствляет товар)
+# 4) Имя файла с изображением данного товара (при парсинге каталога, необходимо так же загружать изображения товара, давать файлам уникальные имена, и записывать имя файла в соответсвуюущую строчку в таблице-файле)
+# 5) Условный идетификатор группы или товара (по которому вы сможете определить, были ли этот товар/группа загружены вашей программой)
+# После того, как первую 1000 товаров ваша программа загрузила, необходимо рассчитать статистику и вывести ее в консоль:
 
+# class search and save catalog in file
 class Sniffer
+  URL = 'http://www.a-yabloko.ru/catalog/'.freeze
 
-  URL = 'http://www.a-yabloko.ru/catalog/'
-
-  Statistics = Struct.new( :group_statistict,
-                           :coef_image,
-                           :min_image_size,
-                           :max_image_size,
-                           :avg_image_size)
+  Statistics = Struct.new(:group_statistict,
+                          :coef_image,
+                          :min_image_size,
+                          :max_image_size,
+                          :avg_image_size)
 
   def initialize
     @results = []
     @page = Mechanize.new
-    @page.history_added = Proc.new { sleep 0.5 }
+    @page.history_added = proc { sleep 0.5 }
     create_catalog_map
   end
 
   def sniff(resourse: '')
-    @page.get(URL+resourse) do |page|
-      @results << page.css('.goods .item a.img').map { |name| "item" + "," + name['title'] + "," + name['rel'] + "," + "#{resourse[0..-2].to_i}" + "," + item_id(name, resourse) }
+    @page.get(URL + resourse.to_s) do |page|
+      page.css('.goods .item a.img').map do |name|
+        @results << ("item,#{name['title']},#{resourse},#{name['rel']}," + name['href'].gsub("/catalog/#{resourse}/goods/", '').to_i.to_s)
+        @page.get(name['rel']).save "./#{name['rel']}"
+      end
     end
   end
 
   def create_catalog_map
     @page.get(URL).search('.sc-desktop table').each do |page|
-      @group = page.css('.root').map { |link| link['href'].gsub("/catalog/",'') }
-      @subgroup = page.css('.ch a').map { |link| link['href'].gsub("/catalog/",'') }
+      #@group = page.css('.root').map do |link|
+      #  @results << ("group, " link['href'].text "#{link['href'].gsub('/catalog/', '').to_i}")
+      # end
+      @group = page.css('.root').map { |link| link['href'].gsub('/catalog/', '').to_i }
+      @subgroup = page.css('.ch a').map { |link| link['href'].gsub('/catalog/', '').to_i }
     end
+    #p @group.sort
+    #p @subgroup.sort
   end
 
   def parse
@@ -44,19 +52,18 @@ class Sniffer
   end
 
   def save
-    File.open('test.txt', 'w+') { |f| f << @results }
+    File.open('test.txt', 'w+') { |f| f.puts(@results) }
   end
-private
-  def item_id(name, resourse)
-    name["href"].gsub("/catalog/#{resourse}goods/",'').to_i.to_s
+
+  def download_image(url)
+
   end
 end
+#a = Sniffer.new
+#p a.group
 
+#a.sniff(resourse: '21/')
+#b = a.sniff(resourse: '45/')
+
+#b.map { |z| p z}
 Sniffer.new.parse
-
-#File.open('test2.txt', 'w+') do |csv_file|
-#  results.each do |row|
-#    csv_file << row
-#  end
-#end
-#dock = File.open('test2.txt', 'w+') { |f| f << page }
